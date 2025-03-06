713c39e706e16fe6a43a0a711ef84b8a932c0920
lkx <lkx@lkx.com>
2025-03-06 13:31:47 +0800

    更新

--- res/root_ui.py
+++ res/root_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'root.ui'
+# Form implementation generated from reading ui file 'D:\code\ICEHelper\res/root.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
